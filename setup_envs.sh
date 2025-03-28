d0447a1f1ef3ff64f7434bdb995fd4b6f102b839
actyp <actypaldos@gmail.com>
2025-03-28 14:27:59 -0400

    Add setup_envs.sh

---
+++ setup_envs.sh
@@ -0,0 +1,15 @@
+module load miniconda
+
+conda create -n vllm python=3.12 -y
+conda activate vllm
+
+pip install vllm
+pip install flashinfer-python -i https://flashinfer.ai/whl/cu124/torch2.6/
+conda deactivate
+
+
+conda create -n nlp python=3.12 -y
+conda activate nlp
+
+pip install langfun[all] --pre
+conda deactivate
