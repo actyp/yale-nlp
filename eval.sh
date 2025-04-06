585222f4c450a9e31daeccc7ff7ee1e47c1de065
actyp <actypaldos@gmail.com>
2025-04-06 00:34:07 -0400

    Minor correction to shell scripts

--- eval.sh
+++ eval.sh
@@ -1,3 +1,4 @@
+set -e
 bigcodebench.syncheck --samples "${1}"
-
 bigcodebench.evaluate --split complete --subset full --samples "${1}"
+set +e
--- setup_envs.sh
+++ setup_envs.sh
@@ -1,5 +1,6 @@
 module load miniconda
 
+# vllm for the server
 conda create -n vllm python=3.12 -y
 conda activate vllm
 
@@ -8,6 +9,7 @@ pip install flashinfer-python -i https://flashinfer.ai/whl/cu124/torch2.6/
 conda deactivate
 
 
+# nlp for the langfun client
 conda create -n nlp python=3.12 -y
 conda activate nlp
 
@@ -16,7 +18,9 @@ pip install datasets
 pip install -U "huggingface_hub[cli]"
 conda deactivate
 
+# bcb for the bigcodebench remote evaluation
 conda create -n bcb python=3.12 -y
 conda activate bcb
+
 pip install bigcodebench --upgrade
 conda deactivate
