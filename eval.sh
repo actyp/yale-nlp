e1757f65f4bc368b42c3390bb070cd31a0def3de
actyp <actypaldos@gmail.com>
2025-04-06 15:47:11 -0400

    Add sanitize, syncheck and then evaluate in eval.sh

--- eval.sh
+++ eval.sh
@@ -1,4 +1,27 @@
+#!/bin/bash
+
+module purge
+module load miniconda
+conda activate bcb
+
+SAMPLES="${1}"
+SAMPLES_NUM="1140"
+
+SANITIZE_SUCCESS="Sanitized ${SAMPLES_NUM} out of ${SAMPLES_NUM} files."
+SYNCHECK_SUCCESS="All ${SAMPLES_NUM} code are compilable!"
+
 set -e
-bigcodebench.syncheck --samples "${1}"
-bigcodebench.evaluate --split complete --subset full --samples "${1}"
+
+# sanitize first
+echo "Sanitizing ${SAMPLES}"
+bigcodebench.sanitize --calibrate True --samples "${SAMPLES}" 2>/dev/null | grep "${SANITIZE_SUCCESS}"
+
+
+SAN_SAMPLES="${SAMPLES/.jsonl/-sanitized-calibrated.jsonl}"
+echo "Synchecking ${SAN_SAMPLES}"
+bigcodebench.syncheck --samples "${SAN_SAMPLES}" 2> /dev/null | grep "${SYNCHECK_SUCCESS}"
+
+echo "Evaluating ${SAN_SAMPLES}"
+bigcodebench.evaluate --split complete --subset full --samples "${SAN_SAMPLES}"
+
 set +e
