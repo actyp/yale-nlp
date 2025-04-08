d6c6feae15f5b6bbec08374e1f47c7baaae4350f
actyp <actypaldos@gmail.com>
2025-04-07 20:50:57 -0400

    Update alloc.sh with all possible gpus as constraint

--- alloc.sh
+++ alloc.sh
@@ -1,5 +1,4 @@
 # info: https://docs.ycrc.yale.edu/clusters/grace/
-# constraint: --constraint a100 | a5000 | rtx3090
 # check gpu: nvidia-smi
 
-salloc -t 6:00:00 --partition gpu_devel --gpus 1 --constraint rtx3090
+salloc -t 6:00:00 --partition gpu_devel --gpus 1 --constraint 'a100|a5000|rtx3090'
