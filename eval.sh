53b29f9aac88ed959875dfede8d2fda850d2e1da
actyp <actypaldos@gmail.com>
2025-04-08 11:44:20 -0400

    Update generate.slurm and eval.sh

--- eval.sh
+++ eval.sh
@@ -10,18 +10,19 @@ SAMPLES_NUM="1140"
 SANITIZE_SUCCESS="Sanitized ${SAMPLES_NUM} out of ${SAMPLES_NUM} files."
 SYNCHECK_SUCCESS="All ${SAMPLES_NUM} code are compilable!"
 
-set -e
+EVAL_LOG="${1}_eval.log"
 
-# sanitize first
 echo "Sanitizing ${SAMPLES}"
-bigcodebench.sanitize --calibrate True --samples "${SAMPLES}" 2>/dev/null | grep "${SANITIZE_SUCCESS}"
-
+bigcodebench.sanitize --calibrate True --samples "${SAMPLES}" 2>"${EVAL_LOG}" | tee "${EVAL_LOG}" | grep "${SANITIZE_SUCCESS}"
+[[ $! -eq 0 ]] || echo "Failed sanitizing ${SAMPLES}"; exit 1
 
 SAN_SAMPLES="${SAMPLES/.jsonl/-sanitized-calibrated.jsonl}"
+[[ -f "${SAN_SAMPLES}" ]] || echo "Failed to create ${SAN_SAMPLES}"; exit 1
+
 echo "Synchecking ${SAN_SAMPLES}"
-bigcodebench.syncheck --samples "${SAN_SAMPLES}" 2> /dev/null | grep "${SYNCHECK_SUCCESS}"
+bigcodebench.syncheck --samples "${SAN_SAMPLES}" 2> "${EVAL_LOG}" | tee "${EVAL_LOG}" | grep "${SYNCHECK_SUCCESS}"
+[[ $! -eq 0 ]] || echo "Failed synchecking ${SAN_SAMPLES}"; exit 1
 
 echo "Evaluating ${SAN_SAMPLES}"
 bigcodebench.evaluate --split complete --subset full --samples "${SAN_SAMPLES}"
-
-set +e
+[[ $! -eq 0 ]] || echo "Failed evaluating ${SAN_SAMPLES}"; exit 1
