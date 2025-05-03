dd52bb3d3f3efe2f954950a6e4d1cbe6716734e6
actyp <actypaldos@gmail.com>
2025-05-03 18:39:59 -0400

    Update setup_envs.sh with pinned versions

--- setup_envs.sh
+++ setup_envs.sh
@@ -4,7 +4,7 @@ module load miniconda
 conda create -n vllm python=3.12 -y
 conda activate vllm
 
-pip install vllm
+pip install vllm==0.8.2
 pip install flashinfer-python -i https://flashinfer.ai/whl/cu124/torch2.6/
 conda deactivate
 
@@ -13,15 +13,16 @@ conda deactivate
 conda create -n nlp python=3.12 -y
 conda activate nlp
 
-pip install langfun[all] --pre
-pip install datasets
-pip install autopep8
-pip install -U "huggingface_hub[cli]"
+pip install langfun[all]==0.1.2.dev202504180804
+pip install autopep8==2.3.2
+pip install datasets==3.5.0
+pip install huggingface_hub[cli]==0.30.2
+pip install matplotlib==3.10.1
 conda deactivate
 
 # bcb for the bigcodebench remote evaluation
 conda create -n bcb python=3.12 -y
 conda activate bcb
 
-pip install bigcodebench --upgrade
+pip install bigcodebench==0.2.5
 conda deactivate
